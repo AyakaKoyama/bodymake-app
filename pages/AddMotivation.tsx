71bb2400c280fe07b39dc2caf0eedb3c8a1c96c0
AyakaKoyama <ayamail2000@gmail.com>
2024-10-23 16:33:59 +0900

    add API_URL

--- pages/AddMotivation.tsx
+++ pages/AddMotivation.tsx
@@ -1,7 +1,7 @@
 //データ挿入処理
 export default async function AddMotivation(value: string) {
   // fetch関数を使用して手順2で作成したAPIエンドポイントにリクエストを送信
-  const res = await fetch("https://api/supabaseFunctions", {
+  const res = await fetch("${process.env.API_URL}/api/supabaseFunctions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
