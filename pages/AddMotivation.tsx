56d22c33882f4f60f6a1637b9280f01f887aa9ec
AyakaKoyama <ayamail2000@gmail.com>
2024-10-23 17:13:04 +0900

    restore-url

--- pages/AddMotivation.tsx
+++ pages/AddMotivation.tsx
@@ -1,14 +1,13 @@
 //データ挿入処理
 export default async function AddMotivation(value: string) {
   // fetch関数を使用して手順2で作成したAPIエンドポイントにリクエストを送信
-  const res = await fetch(`${process.env.API_URL}/api/supabaseFunctions`, {
+  const res = await fetch("/api/supabaseFunctions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ value }),
   });
-  console.log("API_URL:", process.env.API_URL);
   const data = await res.json();
   return data;
 }
